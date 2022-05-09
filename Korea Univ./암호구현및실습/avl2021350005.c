20e0ecf09c90f2bc310631576405a7d9fa5bde8b
hyunsungoh <kyiel1018@korea.ac.kr>
2022-05-09 17:57:51 +0900

    220509

--- Korea Univ./암호구현및실습/avl2021350005.c
+++ Korea Univ./암호구현및실습/avl2021350005.c
@@ -1,12 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define MAX(a,b) (a)
 
-typedef struct node
+int balance;
+
+typedef struct _TreeNode
 {
     int fp;
-    struct node* left;
-    struct node* right;
+    struct _TreeNode* left;
+    struct _TreeNode* right;
 }TreeNode;
+
+
+int Height(TreeNode *node);
+int breakingNode(TreeNode *node);
+TreeNode* new_node(int key);
+TreeNode* insert_node(TreeNode **node, int key);
+void preorder(FILE *file, TreeNode *root);
+void inorder(FILE *file, TreeNode *root);
+
+
+int Height(TreeNode *node)
+{
+    int height = 0;
+    if(node == NULL)
+        return 0;
+    else if(node != NULL)
+        height = 1 + MAX(Height(node->left), Height(node->right));
+
+    return height;
+}
+
+int breakingNode(TreeNode *node)
+{
+    if(node == NULL)
+        return 0;
+    
+    return Height(node->left) - Height(node->right);
+}
 
 
 TreeNode* new_node(int key)
@@ -18,35 +49,36 @@ TreeNode* new_node(int key)
     return temp;
 }
 
-TreeNode* insert_node(TreeNode *node,int key)
+TreeNode* insert_node(TreeNode **node, int key)
 {
-    if(node == NULL)
-        return new_node(key);
-    if(key < node->fp)
-        node->left = insert_node(node->left, key);
-    else if(key > node->fp)
-        node->right = insert_node(node->right, key);
+    if((*node) == NULL)
+        (*node) = new_node(key);
+    if(key < (*node)->fp)
+        (*node)->left = insert_node(&((*node)->left), key);
+    else if(key > (*node)->fp)
+        (*node)->right = insert_node(&((*node)->right), key);
 
-    return node;
+    balance = breakingNode(*node);
+    return *node;
 }
 
-void preorder(TreeNode *root)
+void preorder(FILE *file, TreeNode *root)
 {
     if(root)
     {
-        printf("%d", root->fp);
-        preorder(root->left);
-        preorder(root->right);
+        fprintf(file, "%d ", root->fp);
+        preorder(file, root->left);
+        preorder(file, root->right);
     }
 }
 
-void inorder(TreeNode *root)
+void inorder(FILE *file, TreeNode *root)
 {
     if(root)
     {
-        inorder(root->left);
-        printf("%d", root->fp);
-        inorder(root->right);
+        inorder(file, root->left);
+        fprintf(file, "%d ", root->fp);
+        inorder(file, root->right);
         
     }
 }
@@ -55,30 +87,33 @@ int main()
 {
     TreeNode *root = NULL;
     FILE *fp, *fp2;
-    int num1;
-    int p ,i;
+    char insert[10];
+    int num;
     
 
     fp = fopen("AVL.in.", "r");  // 파일 읽기
+    int ret;
+    fp2 = fopen("AVL.out.", "w"); // 파일 쓰기
+
     if(fp==NULL)
     {
         printf("file open fail\n");
         exit(0);
     }
-    while(1)
+    while(fscanf(fp, "%s%d", insert, &num) != EOF)
     {
-        fscanf(fp, "%d", &num1);
-    }
+        insert_node(&root, num);
 
-
-    fp2 = fopen("AVL.out.", "w");    // 파일 쓰기
-    while(1)
-    {
         fprintf(fp2, "I ");
-        inorder(root);
-        fprintf(fp2, "P ");
-        preorder(root);
-        fprintf(fp2, "BN ");
+        inorder(fp2, root);
+        fprintf(fp2, "\nP ");
+        preorder(fp2, root);
+        fprintf(fp2, "\nBN ");
+        fprintf(fp2, "%d", balance);
+        fprintf(fp2, "\n\n");
+
+        if(ret==EOF)
+            break;
     }
     
 
@@ -86,6 +121,4 @@ int main()
     fclose(fp2);
 
     return 0;
-
-
 }
